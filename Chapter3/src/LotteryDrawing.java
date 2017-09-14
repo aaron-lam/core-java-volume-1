807546629b84fad2015b86f37fd5ecd91839502c
aaronlam <aaronlam1618@gmail.com>
2017-09-13 18:23:09 -0700

    white spaces deleted

--- Chapter3/src/LotteryDrawing.java
+++ Chapter3/src/LotteryDrawing.java
@@ -1,45 +1,38 @@
 import java.util.*;
 
 /**
- * Generate custom amount of random numbers in a custom range
+ * Using arrays to generate custom amount of random numbers in a custom range
  * @version 1.8.0 09-13-2017
  * @author Aaron Lam
  */
 public class LotteryDrawing {
 
     public static void main(String args[]) {
+        //read user input
         Scanner in = new Scanner(System.in);
-
         System.out.print("How many numbers do you need to bet? ");
         int betAmount = in.nextInt();
-
         System.out.print("What is the highest number you can bet? ");
         int highestBetNumber = in.nextInt();
-
         //fill an array with numbers 1, 2, 3, ..., n
         int[] numbers = new int[highestBetNumber];
         for(int i = 0; i < numbers.length; i++)
             numbers[i] = i + 1;
-
         //create an array for bet
         int[] result = new int [betAmount];
         for(int i = 0; i < result.length; i++){
             int randomNum = (int) (Math.random() * highestBetNumber);
-
             //pick the bet at random location of the number array
             result[i] = numbers[randomNum];
-
             //move the last bet into random location of the number array
             numbers[randomNum] = numbers[highestBetNumber-1];
-
             highestBetNumber--;
         }
-
         //Print out random numbers
         Arrays.sort(result);
         System.out.println("Bet the following combination. It will make you rich!");
         for(int i : result)
             System.out.println(i);
-
     }
+
 }
--- Chapter3/src/LotteryOdds.java
+++ Chapter3/src/LotteryOdds.java
@@ -1,14 +1,13 @@
 import java.util.*;
 
 /**
- * Calculate the possibility of getting lottery prize
+ * Using "for loop" to compute the possibility of getting lottery prize
  * @version 1.8.0 09-13-2017
  * @author Aaron Lam
  */
 public class LotteryOdds {
-    /**
-     * compute binomial coefficient n * (n-1) * (n-2) * ... * (n-k+1) / (1 * 2 * ... * k)
-     */
+
+    //compute binomial coefficient n * (n-1) * (n-2) * ... * (n-k+1) / (1 * 2 * ... * k)
     private static int computeBinomialCoefficient(int lotteryOdds, int numNeedToDraw, int numHighestDraw){
         for(int i = 1; i <= numNeedToDraw; i++)
             lotteryOdds = lotteryOdds * (numHighestDraw - i + 1) / i;
@@ -16,16 +15,13 @@ public class LotteryOdds {
     }
 
     public static void main(String args[]){
+        //read user input
         Scanner in = new Scanner(System.in);
-
         System.out.print("How many numbers do you need to draw? ");
         int numNeedToDraw = in.nextInt();
-
         System.out.print("What is the highest number you can draw? ");
         int numHighestDraw = in.nextInt();
-
         int lotteryOdds = computeBinomialCoefficient(1, numNeedToDraw, numHighestDraw);
-
         System.out.println("Your odds are 1 in " + lotteryOdds + ". Good Luck!");
     }
 
